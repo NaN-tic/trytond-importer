0b66c56c177fb0e5bf5d56cc8662825d586a1c9f
Àngel Àlvarez Serra <angel@nan-tic.com>
2022-09-14 20:26:31 +0200

    add party fields #060606 and order_point importer #060634 (#11)

--- __init__.py
+++ __init__.py
@@ -15,6 +15,7 @@ from . import lot
 from . import farm
 from . import invoice
 from . import agronomics
+from . import order_point
 
 def register():
     Pool.register(
@@ -51,12 +52,14 @@ def register():
         depends=['sale_discount'],
         module='importer', type_='model')
     Pool.register(
+        party.ImporterPurchaseDepends,
         purchase.Importer,
         purchase.ImporterPurchase,
         purchase.ImporterProductSupplier,
         depends=['purchase'],
         module='importer', type_='model')
     Pool.register(
+        party.ImporterPartyStockDepends,
         stock.Importer,
         stock.ImporterLocation,
         stock.ImporterStockMove,
@@ -70,6 +73,7 @@ def register():
     Pool.register(
         account.Importer,
         account.ImporterAccountMove,
+        party.ImporterAccountDepends,
         depends=['account'],
         module='importer', type_='model')
     Pool.register(
@@ -96,10 +100,44 @@ def register():
     Pool.register(
         invoice.Importer,
         invoice.ImporterInvoice,
+        party.ImporterPartyInvoiceDepends,
         depends=['account_invoice'],
         module='importer', type_='model')
     Pool.register(
         agronomics.Importer,
         agronomics.ImporterProductAgronomics,
         depends=['agronomics'],
+        module='importer', type_='model')
+    Pool.register(
+        order_point.Importer,
+        order_point.ImporterOrderPoint,
+        depends=['stock_supply'],
+        module='importer', type_='model')
+    Pool.register(
+        party.ImporterCustomerDepends,
+        depends=['party_customer'],
+        module='importer', type_='model')
+    Pool.register(
+        party.ImporterSupplierDepends,
+        depends=['party_supplier'],
+        module='importer', type_='model')
+    Pool.register(
+        party.ImporterCommissionDepends,
+        depends=['commission'],
+        module='importer', type_='model')
+    Pool.register(
+        party.ImporterIncotermDepends,
+        depends=['incoterm'],
+        module='importer', type_='model')
+    Pool.register(
+        party.ImporterIncotermPurchaseDepends,
+        depends=['purchase_incoterm'],
+        module='importer', type_='model')
+    Pool.register(
+        party.ImporterAEATSIIDepends,
+        depends=['aeat_sii'],
+        module='importer', type_='model')
+    Pool.register(
+        party.ImporterCompanyBankDepends,
+        depends=['company_bank'],
         module='importer', type_='model')
