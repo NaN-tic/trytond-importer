bded9ed585e52a16daed4087b63234f03b480471
Raimon Esteve <raimonesteve@gmail.com>
2022-09-22 18:29:27 +0200

    Binary file name

--- importer.py
+++ importer.py
@@ -257,7 +257,8 @@ class Importer(ModelSQL, ModelView):
         })
     binary_data = fields.Binary('Data', states={
             'invisible': Eval('data_source') != 'binary',
-            })
+            }, filename='binary_file_name')
+    binary_file_name = fields.Text('Binary File Name')
     sql_data = fields.Selection([(None, '')], "SQL Queries", states={
         'invisible': ~Eval('data_source').in_(['sql']),
         'required': Eval('data_source').in_(['sql']),
