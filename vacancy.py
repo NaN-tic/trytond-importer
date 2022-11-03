76cfff8edda92fff6b14e332549425e3ea3b0a57
Albert Cervera i Areny <albert@nan-tic.com>
2022-11-03 09:37:01 +0100

    Add Candidate importer.

---
+++ vacancy.py
@@ -0,0 +1,77 @@
+from trytond.model import ModelView, fields
+from trytond.pool import Pool, PoolMeta
+
+
+class ImporterCandidate(ModelView):
+    'Importer Candidate'
+    __name__ = 'importer.candidate'
+
+    party_name = fields.Char("Party Name")
+    phone = fields.Char("Phone")
+    email = fields.Char("E-mail")
+    profile_url = fields.Char("Profile URL")
+    vacancy = fields.Char("Vacancy")
+    phase = fields.Char('Phase')
+    application_method = fields.Char('Application Method')
+    application_url = fields.Char('Application URL')
+
+
+class Importer(metaclass=PoolMeta):
+    __name__ = 'importer'
+
+    @classmethod
+    def _get_methods(cls):
+        methods = super()._get_methods()
+        methods.update({
+                'candidate': {
+                    'string': 'Candidate',
+                    'model': 'importer.candidate',
+                    'chunked': True,
+                    },
+                })
+        return methods
+
+    @classmethod
+    def import_candidate(cls, records):
+        pool = Pool()
+        ApplicationMethod = pool.get('employee.candidate.application_method')
+        Candidate = pool.get('employee.candidate')
+        ContactMechanism = pool.get('party.contact_mechanism')
+        Party = pool.get('party.party')
+        Resume = pool.get('employee.resume')
+        Phase = pool.get('employee.candidate.phase')
+        Vacancy = pool.get('employee.vacancy')
+
+        vacancies = {x.name: x for x in Vacancy.search([])}
+        methods = {x.name: x for x in ApplicationMethod.search([])}
+        phases = {x.name: x for x in Phase.search([])}
+
+        candidates = []
+        for record in records:
+             party = Party()
+             party.name = record.party_name
+             party.save()
+             phone = ContactMechanism()
+             phone.party = party
+             phone.type = 'phone'
+             phone.value = record.phone
+             phone.save()
+             email = ContactMechanism()
+             email.party = party
+             email.type = 'email'
+             email.value = record.email
+             email.save()
+             resume = Resume()
+             resume.party = party
+             resume.url = record.profile_url
+             resume.save()
+             candidate = Candidate()
+             candidate.vacancy = vacancies.get(record.vacancy)
+             candidate.party = party
+             candidate.phase = phases.get(record.phase)
+             candidate.application_method = methods.get(record.application_method)
+             candidate.url = record.application_url
+             candidate.save()
+             candidates.append(candidate)
+        return candidates
+
