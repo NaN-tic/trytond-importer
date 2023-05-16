38125b0adab902cadd0b339bc9c50486fa288ba6
Raimon Esteve <raimonesteve@gmail.com>
2023-05-16 11:06:47 +0200

    Set invoice account only when changing party or dates issue12239

--- invoice.py
+++ invoice.py
@@ -157,7 +157,7 @@ class Importer(metaclass=PoolMeta):
                 invoice.party = party
                 invoice.on_change_type()
                 invoice.on_change_party()
-                invoice.account = invoice.on_change_with_account()
+                invoice._update_account()
                 invoice.journal = journals.get(record.journal)
                 if 'payment_type' in Invoice._fields:
                     invoice.payment_type = None
