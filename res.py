de414ca481c7a9e99fd13bcb629de8b03097f7a1
Albert Cervera i Areny <albert@nan-tic.com>
2023-09-11 01:30:36 +0200

    Register user role importer only when user_role module is activated.

--- res.py
+++ res.py
@@ -21,14 +21,6 @@ class ImporterUser(ModelView):
     company = fields.Char('Company')
 
 
-class ImporterRole(ModelView):
-    'Importer Role'
-    __name__ = 'importer.role'
-
-    name = fields.Char('Name')
-    groups = fields.Char('Groups', help="Comma separated list of group names")
-
-
 class Importer(metaclass=PoolMeta):
     __name__ = 'importer'
 
@@ -39,11 +31,6 @@ class Importer(metaclass=PoolMeta):
                 'user': {
                     'string': 'Users',
                     'model': 'importer.user',
-                    'chunked': True,
-                    },
-                'role': {
-                    'string': 'Roles',
-                    'model': 'importer.role',
                     'chunked': True,
                     },
                 })
@@ -145,37 +132,4 @@ class Importer(metaclass=PoolMeta):
 
         User.save(to_save)
         return to_save
-
-    @classmethod
-    def import_role(cls, records):
-        pool = Pool()
-        Group = pool.get('res.group')
-        Role = pool.get('res.role')
-
-        groups = Group.search([])
-        groups = {x.name: x for x in groups}
-
-        to_save = []
-        for record in records:
-            roles = Role.search([('name', '=', record.name)], limit=1)
-            if roles:
-                role, = roles
-            else:
-                role = Role()
-                role.name = record.name
-
-            if record.groups:
-                groups_to_add = []
-                for group in record.groups.split(','):
-                    if group.strip() not in groups:
-                        raise UserError(gettext('importer.msg_group_not_found',
-                                group=group))
-                    groups_to_add.append(groups[group])
-                role.groups = groups_to_add
-
-            to_save.append(role)
-
-        Role.save(to_save)
-        return to_save
-
 
