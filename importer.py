8ec5f89391f5474f982df30ec9ebf45814aea656
Albert Cervera i Areny <albert@nan-tic.com>
2024-07-30 13:24:24 +0200

    Remove source columns before regenerating them.

--- importer.py
+++ importer.py
@@ -441,6 +441,7 @@ class Importer(ModelSQL, ModelView):
         to_delete = []
         to_save = []
         source_to_save = []
+        source_to_delete = []
         for importer in importers:
             if not importer.model:
                 continue
@@ -468,6 +469,8 @@ class Importer(ModelSQL, ModelView):
             data = Data(importer.data_source, importer.binary_data,
                 importer.text_data, importer.url_data, conn, sql)
             data.load()
+
+            source_to_delete += importer.source_columns
             if data.rows and importer.has_header:
                 # TODO: Source columns should always be created, including
                 # when no headers exist. We should use indices instead in those
@@ -479,6 +482,7 @@ class Importer(ModelSQL, ModelView):
 
         Column.delete(to_delete)
         Column.save(to_save)
+        SourceColumn.delete(source_to_delete)
         SourceColumn.save(source_to_save)
 
     @classmethod
