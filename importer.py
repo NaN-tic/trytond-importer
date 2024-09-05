dca0c2cc130f460b62f0320f2750af94c1ff5f2e
Albert Cervera i Areny <albert@nan-tic.com>
2024-09-05 10:32:07 +0200

    Do not raise an error if no records were imported as this rolls back the transaction and errors would not be stored.

--- importer.py
+++ importer.py
@@ -1351,9 +1351,6 @@ class ImportSample(Wizard):
 
     def do_import_(self, action):
         records = self.record.data_to_records(sample=self.record.sample_size or 100)
-        if not records:
-            raise UserError(gettext('importer.no_records_imported',
-                importer=self.record.rec_name))
 
         models = {}
         for record in records:
