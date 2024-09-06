c1eae64c3c7319fd6fb5b2a9d5d0aa0354777320
Albert Cervera i Areny <albert@nan-tic.com>
2024-09-06 09:34:15 +0200

    Pyflakes fixes.

--- product.py
+++ product.py
@@ -2,7 +2,6 @@ from decimal import Decimal
 from trytond.model import ModelView, fields
 from trytond.modules.product import price_digits, round_price
 from trytond.pool import PoolMeta, Pool
-from trytond.transaction import Transaction
 from .tools import ImporterModel, Cache, Setup
 
 
